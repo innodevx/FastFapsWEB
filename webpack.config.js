743bb859ba4755560e28cdd4912f72fbe21d4b72
innodevx <innodevx@gmail.com>
2025-03-31 05:49:26 -0300

    env config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -19,7 +20,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
-    new Dotenv,
     new webpack.DefinePlugin({
         'process.env.API_URL': JSON.stringify(process.env.API_URL),
         'process.env.PROMPT_DEFAULT': JSON.stringify(process.env.PROMPT_DEFAULT),
